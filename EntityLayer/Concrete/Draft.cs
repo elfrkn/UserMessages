72ad34e519b5677bc9c80da47d9bd2f1ba80e1c3
Elif ÇALIŞ <101409313+elfrkn@users.noreply.github.com>
2024-04-02 09:18:24 +0200

    Add files via upload

---
+++ EntityLayer/Concrete/Draft.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Draft
+    {
+        [Key]
+        public int DraftID { get; set; }
+        public string? SenderName { get; set; }
+        public string? SenderMail { get; set; }
+        public string? ReceiverName { get; set; }
+        public string? ReceiverMail { get; set; }
+        public string? Subject { get; set; }
+        public string? Content { get; set; }
+        public DateTime Date { get; set; }
+        public bool Status { get; set; }
+      
+    }
+}
---
+++ EntityLayer/Concrete/UsersMessage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class UsersMessage
+    {
+        [Key]
+        public int UserMessageID { get; set; }
+        public string? SenderName { get; set; }
+        public string? SenderMail { get; set; }
+        public string? ReceiverName { get; set; }
+        public string? ReceiverMail { get; set; }
+        public string? Subject { get; set; }
+        public string? Content { get; set; }
+        public DateTime Date { get; set; }
+        public bool Status { get; set; }
+        public bool IsRead { get; set; }
+        public bool IsDraft { get; set; }
+    }
+}
